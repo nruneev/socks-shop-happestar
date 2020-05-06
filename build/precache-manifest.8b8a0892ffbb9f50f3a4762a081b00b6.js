ef5f2e0ff23d049cbc3b4832fbba00f5ee382680
nruneev <nruneev@mail.ru>
2020-05-06 15:34:58 +0300

    [First Build]

---
+++ build/precache-manifest.8b8a0892ffbb9f50f3a4762a081b00b6.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a20b57bce706c8cf00d99f843abb051",
+    "url": "/happestar/index.html"
+  },
+  {
+    "revision": "da052df1101d9a021a2d",
+    "url": "/happestar/static/css/main.a8ecabe6.chunk.css"
+  },
+  {
+    "revision": "82d1e755f3e4e2022561",
+    "url": "/happestar/static/js/2.14072470.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/happestar/static/js/2.14072470.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da052df1101d9a021a2d",
+    "url": "/happestar/static/js/main.3ebc991d.chunk.js"
+  },
+  {
+    "revision": "18f7dfe619fa6f51b37e",
+    "url": "/happestar/static/js/runtime-main.495a99c2.js"
+  },
+  {
+    "revision": "917552f71a3cc38101dfbdffa27a3d9d",
+    "url": "/happestar/static/media/attention_1.917552f7.jpg"
+  },
+  {
+    "revision": "23a07233144d24319aaf02d8fe6f5cee",
+    "url": "/happestar/static/media/attention_2.23a07233.jpg"
+  },
+  {
+    "revision": "b82ad60f4eed7961d8354bcaea6c6ce2",
+    "url": "/happestar/static/media/banner_1.b82ad60f.jpg"
+  },
+  {
+    "revision": "cce24f58dcf2cbec69add106987989d8",
+    "url": "/happestar/static/media/banner_2.cce24f58.jpg"
+  },
+  {
+    "revision": "270a6096f731ec80f529647ead2c74bf",
+    "url": "/happestar/static/media/history.270a6096.jpg"
+  },
+  {
+    "revision": "b913a188e9142550598fea42bb08342f",
+    "url": "/happestar/static/media/inst_1.b913a188.jpg"
+  },
+  {
+    "revision": "add1d3a47470369ac46992b74c671165",
+    "url": "/happestar/static/media/inst_10.add1d3a4.jpg"
+  },
+  {
+    "revision": "b69c537e7cea0625d42cb05c0efbd813",
+    "url": "/happestar/static/media/inst_2.b69c537e.jpg"
+  },
+  {
+    "revision": "821882c8b1ad827082e2354c3e2d8287",
+    "url": "/happestar/static/media/inst_3.821882c8.jpg"
+  },
+  {
+    "revision": "920e1a26db0a68df6eb76e783c89da87",
+    "url": "/happestar/static/media/inst_4.920e1a26.jpg"
+  },
+  {
+    "revision": "083d982999408eb309fc1161e61509f1",
+    "url": "/happestar/static/media/inst_5.083d9829.jpg"
+  },
+  {
+    "revision": "7f434e000e145edc3771807e92b81aed",
+    "url": "/happestar/static/media/inst_6.7f434e00.jpg"
+  },
+  {
+    "revision": "2ef6704d9ea1f3000516ae99e710f471",
+    "url": "/happestar/static/media/inst_7.2ef6704d.jpg"
+  },
+  {
+    "revision": "8675c98507c2d7649b984a39d7815213",
+    "url": "/happestar/static/media/inst_8.8675c985.jpg"
+  },
+  {
+    "revision": "31ff15fd327e76e16b9a2e1a8cfb30e4",
+    "url": "/happestar/static/media/inst_9.31ff15fd.jpg"
+  },
+  {
+    "revision": "8d44eed0d6e58684f8807b9886840811",
+    "url": "/happestar/static/media/item_1.8d44eed0.jpg"
+  },
+  {
+    "revision": "beaadec9e2effcecb71055d9aaec29d9",
+    "url": "/happestar/static/media/item_10.beaadec9.jpg"
+  },
+  {
+    "revision": "71838798fdaa8942f34326e1b532badb",
+    "url": "/happestar/static/media/item_2.71838798.jpg"
+  },
+  {
+    "revision": "75b73ddceae37385055fb972ff5e7427",
+    "url": "/happestar/static/media/item_3.75b73ddc.jpg"
+  },
+  {
+    "revision": "4b09346317739e0264c6816ab642f2fc",
+    "url": "/happestar/static/media/item_4.4b093463.jpg"
+  },
+  {
+    "revision": "efb5d6c8aa8584e75f1b0348d330fd56",
+    "url": "/happestar/static/media/item_5.efb5d6c8.jpg"
+  },
+  {
+    "revision": "96c8bf5d209a796b5c9275a23355d166",
+    "url": "/happestar/static/media/item_6.96c8bf5d.jpg"
+  },
+  {
+    "revision": "e096dd51200d067cc15e6a036c896d89",
+    "url": "/happestar/static/media/item_7.e096dd51.jpg"
+  },
+  {
+    "revision": "30e3ec75585c20ac546f28a62bc70cdf",
+    "url": "/happestar/static/media/item_8.30e3ec75.jpg"
+  },
+  {
+    "revision": "b4db295b93342edb656606c9d348358a",
+    "url": "/happestar/static/media/item_9.b4db295b.jpg"
+  },
+  {
+    "revision": "b7099ec4628a8d22a31fe51f7a03f1e8",
+    "url": "/happestar/static/media/logo.b7099ec4.png"
+  },
+  {
+    "revision": "37e39bd9ca85aa899e231217395bfcc7",
+    "url": "/happestar/static/media/museo300-regular-cyrl.37e39bd9.otf"
+  },
+  {
+    "revision": "3dff389b16a0d83f925a7dbd2cacd392",
+    "url": "/happestar/static/media/museo300-regular.3dff389b.otf"
+  },
+  {
+    "revision": "9b2d42557d635b742456a2d5bbcddba9",
+    "url": "/happestar/static/media/packaging.9b2d4255.jpg"
+  },
+  {
+    "revision": "604e7e1186a25917e4dba61b75f95be5",
+    "url": "/happestar/static/media/production.604e7e11.jpg"
+  }
+]);
