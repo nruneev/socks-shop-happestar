8076f4818413ea4dae983b438268954ebefca774
nruneev <nruneev@mail.ru>
2020-05-29 16:44:05 +0300

    [FrontEnd] Redesign Partner Page

--- src/pages/Partner/index.js
+++ src/pages/Partner/index.js
@@ -7,39 +7,36 @@ const Partner = () => {
 
     const steps = [
         {
-            title: "Как сделать заказ?",
+            title: "Шаг 1:",
             text: "Свяжитесь с нами и мы разработаем для Вас эксклюзивный дизайн, а также расскажем об условиях оптовой отгрузки на уже имеющийся каталог.",
         },
         {
-            title: "Как сделать заказ?",
+            title: "Шаг 2:",
             text: "Гибкая система скидок. Простота и удобство в оплате.",
         },
         {
-            title: "Как сделать заказ?",
+            title: "Шаг 3:",
             text: "В разделе «Доставка» вы найдете для себя самый выгодный и быстрый способ получения заказа. А также предложить свой вариант отправки, мы мобильны и рассмотрим предложения.",
         }
     ];
 
-    let [active, setActive] = useState(0);
-
     return (
         <div className='page partner'>
-            <h1>{t('become_a_partner')}</h1>
-            <div className='steps'>
-                {steps.map((step, key) => {
-                        let className = key === active ? 'active' : '';
-                        return (
-                                <div className={'step ' + className} key={key}
-                                    onClick={() => setActive(key)}>
+            <article>
+                <h1>{t('become_a_partner')}</h1>
+                <div className='steps'>
+                    {steps.map((step, key) => {
+                            return (
                                     <div className='content'>
                                         <h1>{step.title}</h1>
-                                        <p>{step.text}</p>
+                                        <p>
+                                            <span>{step.text}</span>
+                                        </p>
                                     </div>
-                                    <div className='number'>{key + 1}</div>
-                                </div>
+                            )}
                         )}
-                    )}
-            </div>
+                </div>
+            </article>
         </div>
     )
 };
