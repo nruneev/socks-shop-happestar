7b054efacfaff5969cce4a1f7743fb0606f42f3f
nruneev <nruneev@mail.ru>
2020-05-29 21:44:58 +0300

    [FrontEnd] Redesign Contact Page

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -11,32 +11,49 @@ const Contacts = () => {
 
     return (
         <div className='contacts page'>
-            <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1999.6107043147874!2d30.353081516224186!3d59.92200797040204!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46963114d45d296b%3A0xa447ad80081097b1!2z0JvQvtGE0YIg0J_RgNC-0LXQutGCINCt0YLQsNC20Lg!5e0!3m2!1sru!2sru!4v1590411006882!5m2!1sru!2sru"
-                width="600" height="450" frameBorder="0" aria-hidden="true" tabIndex="0"/>
             <div className='contacts-container'>
                 <h1>{t('our_contacts')}</h1>
-                <div className='contacts-data'>
-                    <a className='contact-line' href='tel:+7(911)781-31-00'>
-                        <MdPhoneIphone/><label>+7 (911) 781 31 00</label>
-                    </a>
-                    <a className='contact-line' href='mailto: Happestar@mail.ru'>
-                        <IoIosMail/><label>Happestar@mail.ru</label>
-                    </a>
-                    <a className='contact-line' href='https://instagram.com'>
-                        <FaInstagram/><label>Instagram</label>
-                    </a>
-                    <a className='contact-line' href='https://facebook.com'>
-                        <FaFacebookF/><label>Facebook</label>
-                    </a>
-                    <a className='contact-line' href='https://vk.com'>
-                        <FaVk/><label>VK</label>
-                    </a>
-                    <a className='contact-line' href='https://teleg.run/happestar'>
-                        <FaTelegramPlane/><label>Telegram</label>
-                    </a>
+                <div className="tabs__nav">
+                    <a className="tabs__nav-item">Санкт-Петербург</a>
+                </div>
+                <div className="tabs__content">
+                    <div className="shops__item">
+                        <div className="shops__name">Лофт Проект Этажи</div>
+                        <div className="shops__descr">
+                            <p>
+                                <span>Адрес</span>
+                                <span>Лиговский пр., 74, лит. Д, Лофт Проект Этажи</span>
+                            </p>
+                            <p>
+                                <span>Открыто</span>
+                                <span>10:00 – 23:00 </span>
+                            </p>
+                            <p>
+                                <span>Телефон</span>
+                                <span><a href='tel:+7(911)781-31-00'>+7 (911) 781 31 00</a></span>
+                            </p>
+                        </div>
+                    </div>
+                    <div className="shops__item  shops__item--online">
+                        <div className="shops__name">
+                            Интернет-магазин
+                        </div>
+                        <div className="shops__descr">
+                            <p>
+                                <span>Телефон</span>
+                                <span><a href='tel:+7(911)781-31-00'>+7 (911) 781 31 00</a></span>
+                            </p>
+                            <p>
+                                <span>Email</span>
+                                <span><a href='mailto: Happestar@mail.ru'>Happestar@mail.ru</a></span>
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1999.6107043147874!2d30.353081516224186!3d59.92200797040204!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46963114d45d296b%3A0xa447ad80081097b1!2z0JvQvtGE0YIg0J_RgNC-0LXQutGCINCt0YLQsNC20Lg!5e0!3m2!1sru!2sru!4v1590411006882!5m2!1sru!2sru"
+                width="800" height="450" frameBorder="0"/>
         </div>
     );
 };
